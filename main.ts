0ea736c1df19fb18252e83aa7f1d926af2309604
Ransu-ll <59227493+Ransu-ll@users.noreply.github.com>
2021-01-16 14:28:15 +1100

    Clean up unused code + packages, add inline comments

--- main.ts
+++ main.ts
@@ -1,22 +1,21 @@
-let history = [-1.1]
-let x = 0
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    basic.clearScreen()
+    basic.showNumber(history[history.length - 1])
+})
 input.onGesture(Gesture.Shake, function on_gesture_shake() {
-    let roll = randint(0, 20)
-    basic.showNumber(roll)
-    if (history[0] == -1.1) {
-        history[0] = roll
-    } else {
-        history.push(roll)
-    }
     
-    if (history.length == 11) {
+    roll = randint(0, 20)
+    basic.showNumber(roll)
+    history.push(roll)
+    if (history.length == hist_len + 1) {
+        // 
         history.removeAt(0)
     }
     
     pause(500)
     basic.clearScreen()
 })
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
-    basic.clearScreen()
-    basic.showNumber(history[history.length - 1])
-})
+let roll = 0
+let history : number[] = []
+//  Explicitly state type of list.
+let hist_len = 10
