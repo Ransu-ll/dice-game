c495f3ed9fb80588c3baed3a1644057bc88da609
Ransu-ll <59227493+Ransu-ll@users.noreply.github.com>
2021-01-18 02:45:24 +1100

    Create history viewer

--- main.py
+++ main.py
@@ -1,19 +1,55 @@
-def on_button_pressed_ab():
-    basic.clear_screen()
-    basic.show_number(history[len(history) - 1])
-input.on_button_pressed(Button.AB, on_button_pressed_ab)
-
 def on_gesture_shake():
-    global roll
     roll = randint(0, 20)
     basic.show_number(roll)
     history.append(roll)
     if len(history) == hist_len + 1: #
         history.remove_at(0)
-    pause(500)
+    pause(1000)
     basic.clear_screen()
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
+def on_button_pressed_ab():
+    global x
+    if check_history():
+        return
+    else: 
+        basic.clear_screen()
+        while True:
+            display = history[len(history) - x]
+            basic.show_string("#" + x)
+            basic.show_number(display)
+            basic.clear_screen()
+            pause(1000)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_a():
+    global x
+    if check_history():
+        return
+    elif x == 1:
+        x = len(history) - 1
+    else: 
+        x = x - 1
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global x
+    if check_history():
+        return
+    elif x == len(history) - 1:
+        x = 1
+    else:
+        x = x + 1
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def check_history():
+    if len(history) == 0:
+        return True
+    else:
+        return False
+
+x = 1
+display = None
 roll = 0
 history: List[number] = [] # Explicitly state type of list.
 hist_len = 10 # Ensure memory doesn't overflow with this value
--- main.ts
+++ main.ts
@@ -1,10 +1,5 @@
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
-    basic.clearScreen()
-    basic.showNumber(history[history.length - 1])
-})
 input.onGesture(Gesture.Shake, function on_gesture_shake() {
-    
-    roll = randint(0, 20)
+    let roll = randint(0, 20)
     basic.showNumber(roll)
     history.push(roll)
     if (history.length == hist_len + 1) {
@@ -12,9 +7,59 @@ input.onGesture(Gesture.Shake, function on_gesture_shake() {
         history.removeAt(0)
     }
     
-    pause(500)
+    pause(1000)
     basic.clearScreen()
 })
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    let display: number;
+    
+    if (check_history()) {
+        return
+    } else {
+        basic.clearScreen()
+        while (true) {
+            display = history[history.length - x]
+            basic.showString("#" + x)
+            basic.showNumber(display)
+            basic.clearScreen()
+            pause(1000)
+        }
+    }
+    
+})
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    if (check_history()) {
+        return
+    } else if (x == 1) {
+        x = history.length - 1
+    } else {
+        x = x - 1
+    }
+    
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    if (check_history()) {
+        return
+    } else if (x == history.length - 1) {
+        x = 1
+    } else {
+        x = x + 1
+    }
+    
+})
+function check_history(): boolean {
+    if (history.length == 0) {
+        return true
+    } else {
+        return false
+    }
+    
+}
+
+let x = 1
+let display = null
 let roll = 0
 let history : number[] = []
 //  Explicitly state type of list.
